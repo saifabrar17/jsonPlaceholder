a8a1a2925b61d05e49f5a6e2307e93b974b85319
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-02-24 22:46:48 +0600

    fetch data to UI

--- index.js
+++ index.js
@@ -13,5 +13,16 @@ function loadData(){
 function loadUser(){
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
-    .then(res => console.log(res));
+    .then(data => displayUsers(data));
+}
+
+function displayUsers(data){
+
+    const ul = document.getElementById('users');
+    for(const user of data){
+        console.log(user.name);
+        const li = document.createElement('li');
+        li.innerText = user.name;
+        ul.appendChild(li);
+    }
 }
